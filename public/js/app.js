c869b18fe22bfd23c8bbb35a217bf26209c6df9e
chad haselden <chadhaselden@chads-MacBook-Pro.local>
2016-10-21 11:40:22 -0400

    updates to oct-21 11-40

--- public/js/app.js
+++ public/js/app.js
@@ -31,6 +31,19 @@ var controllerRouter = function(){
       }
       switch(currentHashPrefix){
          case "add":
+
+          var objForDatabase = {
+              //  title: showViewSightingPage.title.value,
+            // director: formEl.director.value,
+            // decription: formEl.description.value,
+            // location: formEl.location.value,
+            // locationImg: formEl.locationImg.value
+             }
+          //  })
+          // }
+
+              console.log(objForDatabase);
+
          $.post(' https://quik-spitter-api.herokuapp.com/api/add-siting').then(function(userInput){
 
          })
@@ -38,11 +51,11 @@ var controllerRouter = function(){
             break;
          }
 
-   }
+}
 
 controllerRouter()
 window.addEventListener('hashchange' , controllerRouter)
+    //  $.post(' https://quik-spitter-api.herokuapp.com/api/add-siting').then(function(userInput){
 
 
-
-//showViewSightingPage()
+// showViewSightingPage()
